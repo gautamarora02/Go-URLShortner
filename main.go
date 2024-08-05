6ab38343187b70160a3acbd2fcc660c07060b27e
gautamarora04 <gautamvijayarora@gmail.com>
2024-08-05 20:44:40 +0530

    Server Initialised and Algo completed

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
+// This project is made by Gautam from IIT Bhubaneswar
+// This projects shorten the proivded URL using hash func md5 that uses checksum algo
+
 package main
 
 import (
 	"crypto/md5"
+	"encoding/hex"
+	"errors"
 	"fmt"
+	"net/http"
 	"time"
 )
 
@@ -32,12 +38,39 @@ func generateShortURL(OrignalURL string) string {
 	hasher.Write([]byte(OrignalURL))
 	fmt.Println("hasher: ", hasher)
 	data := hasher.Sum(nil)
+	hash := hex.EncodeToString(data)
+	hash = hash[:8]
+	fmt.Println(hash)
 	fmt.Println(data)
-	return "https://gautam.com"
+	return hash
+}
+
+func CreateURL(OrignalURL string) {
+	hash := generateShortURL(OrignalURL)
+	urlDB[hash] = URL{
+		ID:           hash,
+		OrignalURL:   OrignalURL,
+		CreationDate: time.Now(),
+		ShortURL:     hash,
+	}
+}
+
+func getURL(id string) (URL, error) {
+	url, ok := urlDB[id] // accessing the value from map return two values, value, boolean, whether key is present or not
+	if !ok {
+		return URL{}, errors.New("URL not found")
+	}
+	return url, nil
 }
 
 func main() {
 	fmt.Println("Starting url shortner!!")
 	OrignalURL := "https://google.com"
 	generateShortURL(OrignalURL)
+	fmt.Println("Server starting on port 3000!!")
+	// http.ListenAndServe :: return an error and takes two input, port number and handler func
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		fmt.Println("Error on starting server: ", err)
+	}
 }
